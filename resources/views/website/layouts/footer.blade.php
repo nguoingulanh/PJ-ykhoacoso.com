b0c329f19c494e09d1d4132870f5a69c5a03482f
nguoingulanh <nguoingulanh@gmail.com>
2020-05-22 17:25:48 +0700

    fixed view footer

--- resources/views/website/layouts/footer.blade.php
+++ resources/views/website/layouts/footer.blade.php
@@ -56,7 +56,7 @@
         <div class="row">
             <div class="col-md-12 text-center">
                 <p>
-                    {{GetSetting('site_copyright')}}
+                    {!! GetSetting('site_copyright')!!}
                 </p>
             </div>
         </div>
